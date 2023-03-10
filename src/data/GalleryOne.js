e0abbd8270994361dd8bb84d5ca56ea0ed40f38b
jayendrakamesh <shaassyyy@gmail.com>
2023-03-10 20:33:57 +0530

    2

--- src/data/GalleryOne.js
+++ src/data/GalleryOne.js
@@ -13,6 +13,38 @@ import ImgTwelve from '../images/gallery-one/image-12.jpg'
 import ImgThirteen from '../images/gallery-one/image-13.jpg'
 import ImgFourteen from '../images/gallery-one/image-14.jpeg'
 import ImgFifteen from '../images/gallery-one/image-15.jpeg'
+import ImgSixteen from '../images/gallery-one/image-16.JPG'
+import ImgSeventeen from '../images/gallery-one/image-17.HEIC'
+import ImgEighteen from '../images/gallery-one/image-18.HEIC'
+import ImgNineteen from '../images/gallery-one/image-19.HEIC'
+import ImgTwenty from '../images/gallery-one/image-20.jpeg';
+import ImgTwentyOne from '../images/gallery-one/image-21.jpeg';
+import ImgTwentyTwo from '../images/gallery-one/image-22.jpeg';
+import ImgTwentyThree from '../images/gallery-one/image-23.jpeg';
+import ImgTwentyFour from '../images/gallery-one/image-24.jpeg';
+import ImgTwentyFive from '../images/gallery-one/image-25.jpeg';
+import ImgTwentySix from '../images/gallery-one/image-26.jpeg';
+import ImgTwentySeven from '../images/gallery-one/image-27.jpeg';
+import ImgTwentyEight from '../images/gallery-one/image-28.jpeg';
+import ImgTwentyNine from '../images/gallery-one/image-29.jpeg';
+import ImgFortyEight from '../images/gallery-one/image-48.jpeg';
+import ImgThirty from '../images/gallery-one/image-30.jpg';
+import ImgThirtyOne from '../images/gallery-one/image-31.jpg';
+import ImgThirtyTwo from '../images/gallery-one/image-32.jpg';
+import ImgThirtyThree from '../images/gallery-one/image-33.jpg';
+import ImgThirtyFour from '../images/gallery-one/image-34.jpg';
+import ImgThirtyFive from '../images/gallery-one/image-35.jpg';
+import ImgThirtySix from '../images/gallery-one/image-36.jpg';
+import ImgThirtySeven from '../images/gallery-one/image-37.jpg';
+import ImgThirtyEight from '../images/gallery-one/image-38.jpg';
+import ImgThirtyNine from '../images/gallery-one/image-39.jpg';
+import ImgForty from '../images/gallery-one/image-40.jpg';
+import ImgFortyOne from '../images/gallery-one/image-41.jpg';
+import ImgFortyTwo from '../images/gallery-one/image-42.jpg';
+import ImgFortyThree from '../images/gallery-one/image-43.jpg';
+import ImgFortyFour from '../images/gallery-one/image-44.jpg';
+import ImgFortyFive from '../images/gallery-one/image-45.jpg';
+import ImgFortySix from '../images/gallery-one/image-46.jpg';
 
 import ImgOneMobile from '../images/mobileGallery/image-1.jpg'
 import ImgTwoMobile from '../images/mobileGallery/image-2.jpg'
@@ -29,6 +61,40 @@ import ImgTwelveMobile from '../images/mobileGallery/image-12.jpg'
 import ImgThirteenMobile from '../images/mobileGallery/image-13.jpg'
 import ImgFourteenMobile from '../images/mobileGallery/image-14.jpeg'
 import ImgFifteenMobile from '../images/mobileGallery/image-15.jpeg'
+import ImgSixteenMobile from '../images/mobileGallery/image-16.JPG'
+import ImgSeventeenMobile from '../images/mobileGallery/image-17.HEIC'
+import ImgEighteenMobile from '../images/mobileGallery/image-18.HEIC'
+import ImgNineteenMobile from '../images/mobileGallery/image-19.HEIC'
+import ImgTwentyMobile from '../images/mobileGallery/image-20.jpeg';
+import ImgTwentyOneMobile from '../images/mobileGallery/image-21.jpeg';
+import ImgTwentyTwoMobile from '../images/mobileGallery/image-22.jpeg';
+import ImgTwentyThreeMobile from '../images/mobileGallery/image-23.jpeg';
+import ImgTwentyFourMobile from '../images/mobileGallery/image-24.jpeg';
+import ImgTwentyFiveMobile from '../images/mobileGallery/image-25.jpeg';
+import ImgTwentySixMobile from '../images/mobileGallery/image-26.jpeg';
+import ImgTwentySevenMobile from '../images/mobileGallery/image-27.jpeg';
+import ImgTwentyEightMobile from '../images/mobileGallery/image-28.jpeg';
+import ImgTwentyNineMobile from '../images/mobileGallery/image-29.jpeg';
+import ImgFortyEightMobile from '../images/mobileGallery/image-48.jpeg';
+import ImgThirtyMobile from '../images/mobileGallery/image-30.jpg';
+import ImgThirtyOneMobile from '../images/mobileGallery/image-31.jpg';
+import ImgThirtyTwoMobile from '../images/mobileGallery/image-32.jpg';
+import ImgThirtyThreeMobile from '../images/mobileGallery/image-33.jpg';
+import ImgThirtyFourMobile from '../images/mobileGallery/image-34.jpg';
+import ImgThirtyFiveMobile from '../images/mobileGallery/image-35.jpg';
+import ImgThirtySixMobile from '../images/mobileGallery/image-36.jpg';
+import ImgThirtySevenMobile from '../images/mobileGallery/image-37.jpg';
+import ImgThirtyEightMobile from '../images/mobileGallery/image-38.jpg';
+import ImgThirtyNineMobile from '../images/mobileGallery/image-39.jpg';
+import ImgFortyMobile from '../images/mobileGallery/image-40.jpg';
+import ImgFortyOneMobile from '../images/mobileGallery/image-41.jpg';
+import ImgFortyTwoMobile from '../images/mobileGallery/image-42.jpg';
+import ImgFortyThreeMobile from '../images/mobileGallery/image-43.jpg';
+import ImgFortyFourMobile from '../images/mobileGallery/image-44.jpg';
+import ImgFortyFiveMobile from '../images/mobileGallery/image-45.jpg';
+import ImgFortySixMobile from '../images/mobileGallery/image-46.jpg';
+
+
 
 const GalleryOne = [
   { id: 1, src: ImgOne, alt: 'Georgia', srcMobile: ImgOneMobile },
@@ -46,5 +112,37 @@ const GalleryOne = [
   { id: 13, src: ImgThirteen, alt: 'Georgia', srcMobile: ImgThirteenMobile },
   { id: 14, src: ImgFourteen, alt: 'Georgia', srcMobile: ImgFourteenMobile },
   { id: 15, src: ImgFifteen, alt: 'Georgia', srcMobile: ImgFifteenMobile },
+  { id: 16, src: ImgSixteen, alt: 'Kansas', srcMobile: ImgSixteenMobile },
+  { id: 17, src: ImgSeventeen, alt: 'Kentucky', srcMobile: ImgSeventeenMobile },
+  { id: 18, src: ImgEighteen, alt: 'Louisiana', srcMobile: ImgEighteenMobile },
+  { id: 19, src: ImgNineteen, alt: 'Maine', srcMobile: ImgNineteenMobile },
+  { id: 20, src: ImgTwenty, alt: 'Maryland', srcMobile: ImgTwentyMobile },
+  { id: 21, src: ImgTwentyOne, alt: 'Massachusetts', srcMobile: ImgTwentyOneMobile },
+  { id: 22, src: ImgTwentyTwo, alt: 'Michigan', srcMobile: ImgTwentyTwoMobile },
+  { id: 23, src: ImgTwentyThree, alt: 'Minnesota', srcMobile: ImgTwentyThreeMobile },
+  { id: 24, src: ImgTwentyFour, alt: 'Mississippi', srcMobile: ImgTwentyFourMobile },
+  { id: 25, src: ImgTwentyFive, alt: 'Missouri', srcMobile: ImgTwentyFiveMobile },
+  { id: 26, src: ImgTwentySix, alt: 'Montana', srcMobile: ImgTwentySixMobile },
+  { id: 27, src: ImgTwentySeven, alt: 'Nebraska', srcMobile: ImgTwentySevenMobile },
+  { id: 28, src: ImgTwentyEight, alt: 'Nevada', srcMobile: ImgTwentyEightMobile },
+  { id: 29, src: ImgTwentyNine, alt: 'New Hampshire', srcMobile: ImgTwentyNineMobile },
+  { id: 30, src: ImgThirty, alt: 'New Jersey', srcMobile: ImgThirtyMobile },
+  { id: 31, src: ImgThirtyOne, alt: 'New Mexico', srcMobile: ImgThirtyOneMobile },
+  { id: 32, src: ImgThirtyTwo, alt: 'New York', srcMobile: ImgThirtyTwoMobile },
+  { id: 33, src: ImgThirtyThree, alt: 'North Carolina', srcMobile: ImgThirtyThreeMobile },
+  { id: 34, src: ImgThirtyFour, alt: 'North Dakota', srcMobile: ImgThirtyFourMobile },
+  { id: 35, src: ImgThirtyFive, alt: 'Ohio', srcMobile: ImgThirtyFiveMobile },
+  { id: 36, src: ImgThirtySix, alt: 'Oklahoma', srcMobile: ImgThirtySixMobile },
+  { id: 37, src: ImgThirtySeven, alt: 'Oregon', srcMobile: ImgThirtySevenMobile },
+  { id: 38, src: ImgThirtyEight, alt: 'Pennsylvania', srcMobile: ImgThirtyEightMobile },
+  { id: 39, src: ImgThirtyNine, alt: 'Rhode Island', srcMobile: ImgThirtyNineMobile },
+  { id: 40, src: ImgForty, alt: 'South Carolina', srcMobile: ImgFortyMobile },
+  { id: 41, src: ImgFortyOne, alt: 'South Dakota', srcMobile: ImgFortyOneMobile },
+  { id: 42, src: ImgFortyTwo, alt: 'Tennessee', srcMobile: ImgFortyTwoMobile },
+  { id: 43, src: ImgFortyThree, alt: 'Texas', srcMobile: ImgFortyThreeMobile },
+  { id: 44, src: ImgFortyFour, alt: 'Utah', srcMobile: ImgFortyFourMobile },
+  { id: 45, src: ImgFortyFive, alt: 'Vermont', srcMobile: ImgFortyFiveMobile },
+  { id: 46, src: ImgFortySix, alt: 'Virginia', srcMobile: ImgFortySixMobile },
+  { id: 48, src: ImgFortyEight, alt: 'Virginia', srcMobile: ImgFortyEightMobile },
 ]
 export default GalleryOne
